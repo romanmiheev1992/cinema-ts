8c930894b2174a93e91cbfc08ceead05784c07e7
Roman <romamiheev498@gmail.com>
2022-02-18 14:05:00 +0300

    stoks_page

--- pages/stoks.tsx
+++ pages/stoks.tsx
@@ -1,32 +1,30 @@
 import axios from "axios"
 import { GetStaticProps } from "next"
-import { ImageHeader } from "../components"
-import { NewsPage } from "../components"
 import { link } from "../helpers/links"
 import { MenuInterface, TechnologiesInterface } from "../interfaces/interfaces"
 import { withLayout } from "../Layout/Layout"
+import { StoсksPage } from "../components/StoсksPage/StoсksPage"
+import { ImageHeader } from "../components"
 
-const News = ({menu, header, news}) => {
+ const Stoks = ({menu, stocks, header}:StoksProps ): JSX.Element=> {
 
     return (
         <>
           <ImageHeader/>
-          <NewsPage/>
+          <StoсksPage/>
         </>
     )
 }
 
-export default withLayout(News)
-
-
+export default withLayout(Stoks)
 export const getStaticProps: GetStaticProps = async () => {
     const {data: menu} = await axios.get<MenuInterface[]>(link.menu)
-    const {data: news} = await axios.get<TechnologiesInterface[]>(link.news)
+    const {data: stocks} = await axios.get<TechnologiesInterface[]>(link.stocks)
     const {data: header} = await axios.get<string>(link.mainImage)
     return {
       props: {
         menu,
-        news,
+        stocks,
         header
       }
     }
@@ -35,7 +33,9 @@ export const getStaticProps: GetStaticProps = async () => {
   export interface StoksProps extends Record<string, unknown> {
     menu: MenuInterface[],
     stocks: TechnologiesInterface[],
-    news: TechnologiesInterface[]
+    header: string
   }
+  
+  
   
   
